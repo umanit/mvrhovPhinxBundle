97977e6bfea138a25c3b8d88242b37a6300378d3
GErpeldinger <erpeldinger.g@free.fr>
2021-11-29 11:52:32 +0100

    feat(seed_commands): Add command phinx:seed:run to populate database

---
+++ Command/SeedRunCommand.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Released under the MIT License.
+ *
+ * Copyright (c) 2017 Miha Vrhovnik <miha.vrhovnik@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Umanit\PhinxBundle\Command;
+
+use Exception;
+use Phinx\Console\Command\AbstractCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SeedRunCommand extends AbstractCommand
+{
+    use CommonTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('phinx:seed:run')
+            ->setDescription('Seed the database')
+            ->addOption(
+                '--seed',
+                '-s',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'The seed class(es) to run'
+            )
+            ->setHelp(
+                <<<EOT
+The <info>seed run</info> command runs all available seeds, optionally up to a specific class(es)
+
+<info>phinx seed:run -e development</info>
+<info>phinx seed:run -e development -s MySeeder -s MyOtherSeeder</info>
+
+EOT
+            );
+    }
+
+    /**
+     * Seed the database.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return integer integer 0 on success, or an error code.
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->initialize($input, $output);
+
+        $seeds = $input->getOption('seed');
+
+        // run the migrations
+        $start = \microtime(true);
+        if (\count($seeds) > 0) {
+            foreach ($seeds as $seed) {
+                $this->getManager()->seed('default', $seed);
+            }
+        } else {
+            $this->getManager()->seed('default');
+        }
+        $end = \microtime(true);
+
+        $output->writeln('');
+        $output->writeln('<comment>All Done. Took ' . \sprintf('%.4fs', $end - $start) . '</comment>');
+
+        return self::CODE_SUCCESS;
+    }
+}
